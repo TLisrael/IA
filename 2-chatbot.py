027d85c509c978f2ae7062ec02c981d3b75adc2b
Thainá Israel <thainaluizaisrael@gmail.com>
2025-08-11 19:16:13 -0300

    Chatbot de textos via terminal sem interface

---
+++ 2-chatbot.py
@@ -0,0 +1,33 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+client = openai.Client(api_key=os.getenv("OPENAI_API_KEY"))
+
+def geracao_texto(mensagens):
+    resposta = client.chat.completions.create(
+        messages=mensagens,
+        model="gpt-4o-mini",
+        temperature=0,
+        max_tokens=1000,
+        stream=True
+    )
+    print("Bot: ", end='')
+    texto_completo = ""
+    for resposta_stream in resposta:
+        texto = resposta_stream.choices[0].delta.content
+        if texto:
+            print(texto, end="")
+            texto_completo += texto
+    print()
+    mensagens.append({"role": "assistant", "content": texto_completo})
+    return mensagens
+
+if __name__ == "__main__":
+    print("Bem vindo ao ChatBot 🤖")
+    mensagens = []
+    while True:
+        in_user = input("User: ")
+        mensagens.append({"role":"user", "content": in_user})
+        mensagens = geracao_texto(mensagens)
